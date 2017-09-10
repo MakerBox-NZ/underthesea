cbf940257be1b1aa6b4dd9223d0e9f4dcef5cbd2
Cyanide Cupcake <jlweichler@gmail.com>
2017-09-10 16:40:56 +1200

    Clean Up Code
    • Removed unnecessary artefacts from sample code
    • Added appropriate comments
    • Simplified code (less functions)

--- testFade_neopixel/testFade_neopixel.ino
+++ testFade_neopixel/testFade_neopixel.ino
@@ -1,41 +1,30 @@
-#include <Adafruit_NeoPixel.h>
-#define PIN 4
-#define LED_COUNT 10
-// Parameter 1 = number of pixels in pixels
-// Parameter 2 = pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+#include <Adafruit_NeoPixel.h> //imports NeoPixel library
+#define PIN 4  //pin number
+#define LED_COUNT 10 //number of LEDs in strip
+
+//(Number of pixels attached to Arduino, pin connected to NeoPixels, third parameter may need changed on older NeoPixel strips)
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  pixels.begin();
-  pixels.show(); // Initialize all pixels to 'off'
+  pixels.begin();//Initalise NeoPixel library
+  pixels.show(); // Initialise all pixels to 'off'
 }
+
 
 // *** CHANGE COLOURS IN VOID LOOP ***
 void loop() { 
   int randNumber = random(5, 30); //randomises delay
-  FadeInOut(0, 0, 255, randNumber); //red, green, blue
-  delay(500);
-  FadeInOut(0, 255, 0, randNumber);
+
+  //Fade takes RGB values, from (0,0,0) up to (255,255,255)
+  Fade(0, 150, 150, randNumber); //red, green, blue
+  Fade(100, 0, 50, randNumber);
+  delay(20);
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-void FadeInOut(byte red, byte green, byte blue, int randNumber){
+//*** DO NOT CHANGE CODE BELOW***
+//Increase colour values by +1
+void Fade(byte red, byte green, byte blue, int randNumber){
   float r, g, b;
       
   for(int k = 0; k < 256; k=k+1) { 
@@ -43,43 +32,30 @@ void FadeInOut(byte red, byte green, byte blue, int randNumber){
     g = (k/256.0)*green;
     b = (k/256.0)*blue;
     setAll(r,g,b);
-    showPixels();
-    delay(randNumber);
+    pixels.show();
+    delay(randNumber); //randomise duration of fade
   }
-  
 
-       
+//Decrease colour values by -2
   for(int k = 255; k >= 0; k=k-2) {
     r = (k/256.0)*red;
     g = (k/256.0)*green;
     b = (k/256.0)*blue;
     setAll(r,g,b);
-    showPixels();
-    delay(randNumber);
+    pixels.show();
+    delay(randNumber); //randomise duration of fade
   }
 
 }
 
-// *** REPLACE TO HERE ***
 
-void showPixels() {
- #ifdef ADAFRUIT_NEOPIXEL_H 
-   // NeoPixel
-   pixels.show();
- #endif
-}
-
-void setPixel(int Pixel, byte red, byte green, byte blue) {
- #ifdef ADAFRUIT_NEOPIXEL_H 
-   // NeoPixel
-   pixels.setPixelColor(Pixel, pixels.Color(red, green, blue));
- #endif
-
-}
-
+//Sets all NewPixels to on
 void setAll(byte red, byte green, byte blue) {
+  //First NeoPixel is 0, second is 1, and so on)
   for(int i = 0; i < LED_COUNT; i++ ) {
-    setPixel(i, red, green, blue); 
+    pixels.setPixelColor (i, pixels.Color(red, green, blue)); 
+    //pixels.show(); //place show outside of for loop to set as group
+    //delay(10); //remove delay to set as group
   }
-  showPixels();
+   pixels.show();
 }
